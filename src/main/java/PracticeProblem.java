569246ef3a9b6156f31513e6f41aee88478257ba
JasonWu-ux <jasonwu544@gmail.com>
2026-03-05 14:44:07 +0000

    finished questions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,32 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+		public static boolean validIndex(int[] array, int index) {
+		try {
+			int value = array[index];
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
 
-	
+
+	public static int divide(int a, int b) {
+		try {
+			return a / b;
+		} catch (ArithmeticException e) {
+			System.out.println("Cannot divide by zero.");
+			return 0; 
+			
+		}
+	}
+
+	public static int safeConvertStringtoInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 
 }
